d88b26d78cfd07742ac1f2c703ec017d5dd1129d
mrjhong <jfdg206@gmail.com>
2022-09-15 05:12:41 -0500

    cambio mejora

--- src/components/statics/Navbar.js
+++ src/components/statics/Navbar.js
@@ -53,9 +53,6 @@ export class Navbar extends Component {
                         </div>
                     </nav>
 
-                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
                     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                         <div class="offcanvas-header">
                             <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
